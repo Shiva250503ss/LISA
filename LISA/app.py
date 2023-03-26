95069f23fe24d183d2d78b0f5e94de20ee9a900d
Shiva250503ss <105617004+Shiva250503ss@users.noreply.github.com>
2023-03-26 23:52:56 +0530

    Update app.py

--- LISA/app.py
+++ LISA/app.py
@@ -28,7 +28,7 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
-import docx
+
 from docx import Document
 
 summarized_text = ""
