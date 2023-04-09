8950b3005901dd81a23f6feadda5fc9cd8a41ef4
Shiva250503ss <105617004+Shiva250503ss@users.noreply.github.com>
2023-04-09 15:28:43 +0530

    otp verification

--- L&T_LISA/app.py
+++ L&T_LISA/app.py
@@ -1,10 +1,19 @@
 from flask import *
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
+import os
+import smtplib
+import ssl
+import math
+import random
+
+smtp_port = 587
+smtp_server = "smtp.gmail.com"
+images=os.path.join('static','images')
+otp=""
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your_secret_string"
-
 
 def dbconnect():
     con = sqlite3.connect("users.db")
@@ -13,6 +22,7 @@ def dbconnect():
     cur.execute("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT NOT NULL, message TEXT NOT NULL)")
     con.commit()
     return cur, con
+
 
 @app.route('/')
 def index():
@@ -43,6 +53,66 @@ def login():
         else:
             return jsonify({'msg': 'Invalid email or password',
                             'status': 320})
+        
+def OTP():
+    digits = "0123456789"
+    OTP = ""
+    for i in range(6):
+        OTP += digits[math.floor(random.random() * 10)]
+    return OTP
+
+@app.route('/otp', methods=['POST'])
+def otpt():
+    global otp
+    # global simple_email_context
+    data = request.get_json()
+    email = data['email']
+    conn_cred = sqlite3.connect('cred.db')
+
+    # Create a cursor
+    cursor_cred = conn_cred.cursor()
+
+        # Execute an SQL command to retrieve the first row from the user table
+    cursor_cred.execute('SELECT * FROM user LIMIT 1')
+
+        # Fetch the first row and print it
+    row = cursor_cred.fetchone()
+    email_from=row[1]
+    decrypted=row[2]
+
+        # Close the connection
+    conn_cred.close()
+    try:
+        otp = OTP() 
+        otpp= otp + " is your OTP!"
+        subject = f"Welcome th LISA!"
+        message = f"{otpp}.\nPlease do not share it with anyone."
+        simple_email_context = ssl.create_default_context()
+        print("Connecting to server...")
+        tie_server = smtplib.SMTP(smtp_server,smtp_port)
+        tie_server.starttls(context=simple_email_context)
+        tie_server.login(email_from, decrypted)
+        print()
+        print(f"Sending email to - {email}")
+        tie_server.sendmail(email_from, email, f"Subject: {subject}\n\n{message}")
+        print(f"Email successfully sent to - {email}")
+        return "success"
+    except Exception as e:
+        print(e)
+        return "failure"
+    
+@app.route('/otp1', methods=['POST'])
+def otp1():
+    global otp
+    # global simple_email_context
+    data = request.get_json()
+    otp_1 = data['otp']
+    print(otp_1)
+    print(otp_1,otp)
+    if (otp_1==otp):
+        return "Success"
+    else:
+        return "Failure"
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
